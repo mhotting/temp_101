f0c20fcdff6003610fda9dfbbabe57573532de28
mhotting <madric.hottinger.101@gmail.com>
2019-02-11 13:55:29 +0100

    Added TP2

---
+++ OCR/TP2_minichat/inc/header.php
@@ -0,0 +1,8 @@
+    <header class="header">
+        <table>
+            <tr>
+                <td><a href="./index.php">Accueil</a></td>
+                <td><a href="./signup.php">Créer un compte</a></td>
+            </tr>
+        </table>
+    </header>
---
+++ OCR/TP2_minichat/signup.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Mini-chat: Accueil</title>
+    <link rel="stylesheet" href="./styles/style.css">
+</head>
+<body>
+    <?php require_once("./inc/header.php"); ?>
+    <div class="content">
+        <fieldset>
+            <legend>Créer un compte</legend>
+            <form action="./controller/index.php" method="post">
+            <table>
+                <tr>
+                    <td>Votre pseudo:</td>
+                    <td><input type="text" name="pseudo" placeholder="Saisir pseudo"></td>
+                </tr>
+                <tr>
+                    <td>Votre mot de passe:</td>
+                    <td><input type="password" name="pwd" placeholder="Saisir mot de passe"></td>
+                </tr>
+                <tr>
+                    <td>Confirmer votre mot de passe:</td>
+                    <td><input type="password" name="confirm_pwd" placeholder="Saisir mot de passe"></td>
+                </tr>
+            </table>
+            </form>
+        </fieldset>
+    </div>
+</body>
+</html>
