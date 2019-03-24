f1397ad0af69227a60713249521eeaa986248800
mhotting <madric.hottinger.101@gmail.com>
2019-03-24 19:09:56 +0100

    Correct version

--- NODEJS/0_EXERCISES/PRACTICE_0_CHAT/controllers/index.js
+++ NODEJS/0_EXERCISES/PRACTICE_0_CHAT/controllers/index.js
@@ -1,5 +1,13 @@
 // Index controller
+const db = require('./../util/database');
 
 exports.getIndex = (req, res, next) => {
-    res.render('index', { pageTitle: 'Accueil' });
+    db.execute('SHOW TABLES')
+        .then((result) => {
+            console.log(result);
+            res.render('index', { pageTitle: 'Accueil' });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 };
